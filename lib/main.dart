4f22dc3bdd8d52057d486939ee488fc85aba6907
adiboy-23 <kadiansahab123gmail.com>
2024-10-31 22:48:39 +0530

    add sign in and register

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:food_del/auth/login_or_register.dart';
+import 'package:food_del/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LoginOrRegister(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+    );
+  }
+}
